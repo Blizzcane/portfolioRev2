a34099e2e7f80f4e4f3e953e28a4dcc84885b458
Blizzcane <blizzcane@gmail.com>
2023-04-20 20:48:38 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from 'react';
 import './App.css';
+import Section from './Section';
 
 function App() {
   return (
-    <div className="App">
- 
+    <div className="container">
+      <Section title="Home">
+        <p>Welcome to my website!</p>
+      </Section>
+      <Section title="About">
+        <p>Learn more about me and my background.</p>
+      </Section>
+      <Section title="Skills">
+        <p>Here are some of the skills I have.</p>
+      </Section>
+      <Section title="Projects">
+        <p>Check out some of my recent projects.</p>
+      </Section>
+      <Section title="Contact">
+        <p>Get in touch with me.</p>
+      </Section>
     </div>
   );
 }
