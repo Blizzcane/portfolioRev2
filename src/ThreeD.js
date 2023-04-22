c76dd5157711aed39d6069a3f56f41a205a15e04
Blizzcane <blizzcane@gmail.com>
2023-04-22 17:33:24 -0500

    styled spline

--- src/ThreeD.js
+++ src/ThreeD.js
@@ -1,10 +1,19 @@
-import Spline from '@splinetool/react-spline';
+import Spline from "@splinetool/react-spline";
 
- const ThreeD =() =>{
+const ThreeD = () => {
   return (
-    <Spline scene="https://prod.spline.design/Hw2DcW9yMjXhTl5u/scene.splinecode" />
-  );
-}
+<Spline 
+  style={{ 
+    width: "300px", 
+    height: "200px", 
+    border: "1px solid #ccc",
+    borderRadius: "15px",
+    overflow: "hidden"
+  }}  
+  scene="https://prod.spline.design/Hw2DcW9yMjXhTl5u/scene.splinecode" 
+/>
 
+  );
+};
 
 export default ThreeD;
